9be3db4c0407b6dc4cc12b4d823707758c8b34cb
rarg08 <rarg08@gmail.com>
2023-09-19 16:45:05 -0600

    cambios a endpoints y server

--- routes/mostPop.js
+++ routes/mostPop.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models'); // Import your User model
 
-router.get('/mostPopular', (req, res) => {
-    res.sendFile(__dirname + ''); //
+router.get('/', async (req, res, next) => {
+    res.render('./views/mostPop.handlebars', { title: 'Most Popular'})
     });
 
 
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const sequelize =require('./config/connection');
 const controllers = require('./controllers');
 const exphbs = require('express-handlebars');
 const userRoutes = require('./controllers/api/userRoutes');
-
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+const mostPop = require('./routes/mostPop');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -16,8 +20,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app/use(cookieParser());
+app.use(cookieParser());
 app.use(controllers);
+app.use('/mostPop', mostPop);
 app.use(session({
   key: 'user_id',
   secret: 'travelplans',
