372dcd74a72a37b1f29cd3ffb1d277c26cea73a2
rarg08 <rarg08@gmail.com>
2023-09-19 15:00:44 -0600

    Cambios de rutas y agregado de session en user route api

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const controllers = require('./controllers');
 const exphbs = require('express-handlebars');
 const userRoutes = require('./controllers/api/userRoutes');
 const routes = require('./controllers');
-
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path'); 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,9 +19,18 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app/use(cookieParser());
 app.use(controllers);
+app.use(session({
+  key: 'user_id',
+  secret: 'travelplans',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
 
+}))
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port:  http://localhost:${PORT}`));
 });
