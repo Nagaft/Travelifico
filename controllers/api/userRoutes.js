c77c8f6a0ac9947786da95faeac6d3561808a898
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-09-19 19:24:09 -0600

    deleting old password reset

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,35 +22,5 @@ res.status(200).json(userData);
   }
 });
 
-// router.get('/reset-password', (req, res) => {
-//   res.render('reset-password', { resetLink: `${baseURL}/reset-password` });
-// });
-
-// router.post('/reset-password', async (req, res) => {
-//   const userEmail = req.body.email;
-
-//   const resetToken = crypto.randomBytes(20).toString('hex');
-  
-//   // Store the reset token and associated email in your database
-//   // (You'll need a database for this)
-
-//   const resetLink = `${baseURL}/reset-password/${resetToken}`;
-
-//   const msg = {
-//     to: userEmail,
-//     from: 'travelificoagency@gmail.com', // Replace with your sender email
-//     subject: 'Password Reset',
-//     html: `Click <a href="${resetLink}">here</a> to reset your password.`,
-//   };
-
-//   try {
-//     await sgMail.send(msg);
-//     console.log('Password reset email sent');
-//     res.redirect('/login');
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 
 module.exports = router;
