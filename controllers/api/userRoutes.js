acce077ac5823c4e80b5c19d65ac9ddc95f031d3
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-09-19 19:22:13 -0600

    update password.reset

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,38 +2,55 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 
-// Define the route for displaying the registration form
-router.get('/create-account', (req, res) => {
-    res.sendFile(__dirname + '/views/register.html');
+router.get('../create-account', (req, res) => {
+  res.sendFile(__dirname + '../views/register.html');
 });
+console.log(User)
 
-// Handle the registration form submission
 router.post('/register', async (req, res) => {
-    try {
-        // Create a new user using data from the request body
-        const userData = await User.create(req.body);
-        console.log(userData);
-        res.status(200).json(userData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Registration failed. Please try again.');
-    }
+  try {
+    // const { email, password } = req.body;
+    // console.log('registro exitoso');
+const userData =  await User.create(req.body);
+console.log (userData);
+
+res.status(200).json(userData);
+    // res.redirect('/success');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Registration failed. Please try again.');
+  }
 });
 
-// Define the route for displaying the user profile
-router.get('/profile', (req, res) => {
-    // Fetch the user's data from your database or wherever it's stored
-    // For this example, we'll assume you have a user object with an 'email' property
-    const user = {
-        email: 'user@example.com', // Replace with the user's actual email
-        // Add other user data as needed
-    };
+// router.get('/reset-password', (req, res) => {
+//   res.render('reset-password', { resetLink: `${baseURL}/reset-password` });
+// });
 
-    // Render the profile.html template and pass user data to it
-    res.render('profile', { user });
-});
+// router.post('/reset-password', async (req, res) => {
+//   const userEmail = req.body.email;
 
-// Placeholder for other routes
-// ...
+//   const resetToken = crypto.randomBytes(20).toString('hex');
+  
+//   // Store the reset token and associated email in your database
+//   // (You'll need a database for this)
+
+//   const resetLink = `${baseURL}/reset-password/${resetToken}`;
+
+//   const msg = {
+//     to: userEmail,
+//     from: 'travelificoagency@gmail.com', // Replace with your sender email
+//     subject: 'Password Reset',
+//     html: `Click <a href="${resetLink}">here</a> to reset your password.`,
+//   };
+
+//   try {
+//     await sgMail.send(msg);
+//     console.log('Password reset email sent');
+//     res.redirect('/login');
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 module.exports = router;
