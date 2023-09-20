78b9286bca8eaa6be8eee9f7dc15bc407dc24935
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-09-20 15:53:25 -0600

    fix syntax in user

--- models/User.js
+++ models/User.js
@@ -12,45 +12,43 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true, // Ensure the email format is valid
+        isEmail: true,
       },
     },
-
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [8], // Ensure the password is at least 8 characters long
       },
     },
-
     phoneNumber: {
       type: DataTypes.STRING, // Use DataTypes.STRING for phone numbers
       allowNull: false,
       validate: {
-        is: /^\d{10}$/ // Use the "is" validation for custom regular expression validation
+        is: /^\d{10}$/, // Use the "is" validation for custom regular expression validation for phone numbers
       },
     }, 
+  },
+  {
+
     hooks: {
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
-      }
+      },
     },
-    {
- main
-    sequelize,
-    modelName: 'user',
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
+    sequelize, // Use the sequelize instance you imported
+    modelName: 'user', // Set the model name
+    timestamps: false, // Disable timestamps (createdAt and updatedAt columns)
+    freezeTableName: true, // Use the model name as the table name
+    underscored: true, // Use snake_case for column names
   }
 );
-//comments
+
 module.exports = User;
