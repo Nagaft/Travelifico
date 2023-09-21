32d35169eedf8ca75ce99f154ff906f0295f0fee
Alejandro Larios <alexlarios70@hotmail.com>
2023-09-20 20:25:20 -0600

    cambios al fetch del api de yelp y arreglos para que corra el server

--- controllers/yelpController.js
+++ controllers/yelpController.js
@@ -1,37 +1,36 @@
 // controllers/yelpController.js
-const fetch = require('node-fetch'); // Asegúrate de tener 'node-fetch' instalado
-
-const API_KEY = Totunefii-aLUwUNCs3ubMZY95KqUlsDbEOtAafQOx0OtSiIyGy6J-rkleGHeoM43BVp24H3HJ2CmZjaKD9xY-wQ5kCRijbeh_SN6z-kiXGecXfq1i6XsPAaH2cKZXYx
+const API_KEY = 'Totunefii-aLUwUNCs3ubMZY95KqUlsDbEOtAafQOx0OtSiIyGy6J-rkleGHeoM43BVp24H3HJ2CmZjaKD9xY-wQ5kCRijbeh_SN6z-kiXGecXfq1i6XsPAaH2cKZXYx'
 ; 
 
-exports.searchRestaurants = async (req, res, next) => {
-  try {
-    const { city, travelDate, numberOfPersons, favoriteFood } = req.body;
-
-    // Construye la URL de la API de Yelp para buscar restaurantes
+import('node-fetch')
+  .then((data) => {
+    const fetch = data.default;
     const yelpApiUrl = `https://api.yelp.com/v3/businesses/search?location=${city}&term=${favoriteFood}&limit=10`;
 
-    const response = await fetch(yelpApiUrl, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-      },
-    });
+    fetch (yelpApiUrl,
+      {
+        method:'GET',
+        headers:{
+          Authorization: `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) =>{
+        if(!response.ok){
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the JSON response
+      })
+      .then((yelpData) => {
+        // Handle and use the Yelp API data
+        console.log('Yelp API response:', yelpData);
+      })
+      .catch((error) => {
+        console.error('Error fetching data from Yelp API:', error);
+      });
+      
+  })
+  .catch((error) => {
+    console.error('Error importing fetch:', error);
+  });
 
-    if (!response.ok) {
-      throw new Error('Error al buscar restaurantes en Yelp');
-    }
-
-    const restaurantData = await response.json();
-
-    // Renderiza una vista o envía los datos como respuesta JSON
-   // res.status(200).json(restaurantData);
-   req.yelpinfo=restaurantData
-    next();
-  } catch (error) {
-    console.error('Error al buscar restaurantes en Yelp:', error);
-    res.status(500).json({ error: 'Hubo un error al buscar restaurantes en Yelp' });
-  }
-};
-
-//
