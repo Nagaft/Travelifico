174deb22f5d2d8082a995eb4dc29599ca757117c
Noé Antonio García Flores <135068393+Nagaft@users.noreply.github.com>
2023-09-20 18:06:56 -0600

    only for update

--- models/user.js
+++ models/user.js
@@ -1,28 +1,49 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../sequelize');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-const User = sequelize.define('User', {
-  nombreUsuario: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true, 
-    validate: {
-      isEmail: true, 
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-  },
-  contraseña: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  points: {
-    type: DataTypes.INTEGER, 
-    defaultValue: 0, 
-  },
-});
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true, // Ensure the email format is valid
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  }, 
+{
+  hooks: {
+    async beforeCreate(newUserData) {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
+    }
+  }
+},
+  {
+    sequelize,
+    modelName: 'user',
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+  }
+);
 
 module.exports = User;
