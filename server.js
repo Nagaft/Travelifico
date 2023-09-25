a519691e9e63bf8bf2f8487106f4897403c4b754
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-09-24 18:54:18 -0600

    working sms api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const userRoutes = require('./controllers/api/userRoutes');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
-// const mostPop = require('./routes/mostPop');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
@@ -20,16 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(controllers);
-// app.use('./mostPop', mostPop);
-// app.use(session({
-//   key: 'user_id',
-//   secret: 'travelplans',
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//     expires: 600000
-//   }
-// }))
+
+
 app.listen(PORT, () => {
   console.log(`Now listening on port:  http://localhost:${PORT}`);
 });
