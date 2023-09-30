3c08b66fd9227c1c5136a4e1b46de6170f6e61fc
Alejandro Larios <alexlarios70@hotmail.com>
2023-09-29 21:41:13 -0600

    changes  in connections

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,11 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  process.env.DATABASE_URL, 
   {
     host:'localhost',
-    dialect: 'mysql', 
-    port: 3306,
+    dialect: process.env.DB_DIALECT || 'mysql', 
+    port: process.env.DB_PORT || 3306,
   }
 );
 
