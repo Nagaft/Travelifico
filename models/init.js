746b2bc098066b2b3d2c44f6c7d01199564bd481
Alejandro Larios <alexlarios70@hotmail.com>
2023-09-30 13:55:30 -0600

    - Actualizar init para usar db uri

--- models/init.js
+++ models/init.js
@@ -1,11 +1,20 @@
 const msql = require("mysql2/promise");
+const env_parse = require('dotenv').config();
+if (env_parse.error) {
+    throw env_parse.error;
+} else {
+    console.log(env_parse.parsed)
+}
 
 const createDb = async () => {
-    const conn = await msql.createConnection({
-        user: "root",
-        password: "Telecaster12",
-        host: "localhost"
-    });
+
+    const conn = process.env.DATABASE_URL
+        ? await msql.createConnection(process.env.DATABASE_URL)
+        : await msql.createConnection({
+            user: "root",
+            password: "Telecaster12",
+            host: "localhost"
+        });
 
     const created = await conn.execute("CREATE DATABASE IF NOT EXISTS travelifico_db");
     return created;
